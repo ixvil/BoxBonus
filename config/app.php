b83287ed8775165b0de424684211a439865096cf
Шипин Алексей <shipin_a@terminal.lft>
2016-09-22 15:31:45 +0300

    Added Model generator

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
+
     ],
 
     /*
