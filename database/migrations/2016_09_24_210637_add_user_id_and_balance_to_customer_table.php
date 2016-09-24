4ea0e341b0c22ab47a93c777a1c709467f01970f
ixvil <alexey.shipin@gmail.com>
2016-09-25 00:16:45 +0300

    Added migration for customers table

---
+++ database/migrations/2016_09_24_210637_add_user_id_and_balance_to_customer_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdAndBalanceToCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customers', function ($table) {
+            $table->integer('balance');
+            $table->integer('user_id');
+
+            $table->foreign('user_id', 'fk_customers_users1')->references('id')->on('users')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customers', function ($table) {
+            $table->dropForeign('fk_customers_users1');
+            $table->dropColumn(array('balance', 'user_id'));
+        });
+    }
+}
