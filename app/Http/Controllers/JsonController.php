2b026d8f2ec4f2ac29eb14d9476ffe3f694594f1
Alexey Shipin <alexey.shipin@gmail.com>
2016-10-02 12:53:20 +0300

    Json API debug

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -9,8 +9,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Schemas\SchemaFactory;
 use App\Schemas\UserSchema;
-use App\View;
+
+use Illuminate\Contracts\View\View;
 use Neomerx\JsonApi\Encoder\Encoder;
 use Neomerx\JsonApi\Encoder\EncoderOptions;
 
@@ -26,10 +28,8 @@ class JsonController extends Controller
     {
         $user = User::find($userId);
 
-        $userSchema = new UserSchema();
-
         $encoder = Encoder::instance([
-            User::class => $userSchema
+            User::class => UserSchema::class
         ], new EncoderOptions(JSON_PRETTY_PRINT, 'http://example.com/api/v1'));
 
         $json = $encoder->encodeData($user);
